4da6d0e1da2a509d93f37465c5f7c9dc623cc396
t0johnso <tylerjdesigner@gmail.com>
2019-02-22 09:15:27 -0700

    host dom element tests

--- projects/ng-mat-tester/src/app/app.component.spec.ts
+++ projects/ng-mat-tester/src/app/app.component.spec.ts
@@ -1,7 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>,
+  app:AppComponent,
+  appNativeElement: Element;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +14,29 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    appNativeElement = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ng-mat-tester'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Angular Material Interactions Dialog');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Material Interactions Dialog!');
+    expect(appNativeElement.querySelector('h1').textContent).toContain('Welcome to Angular Material Interactions Dialog!');
   });
+
+  it('alert, prompt, and confirm buttons should exist', () => {
+    expect(appNativeElement.querySelector('button[name="alert"]')).toBeTruthy();
+    expect(appNativeElement.querySelector('button[name="prompt"]')).toBeTruthy();
+    expect(appNativeElement.querySelector('button[name="confirm"]')).toBeTruthy();
+  });
+
 });
