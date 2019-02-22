4d6ff541f76345b9364e0faf6bf5c7d33b702e2d
t0johnso <tylerjdesigner@gmail.com>
2019-02-22 12:10:43 -0700

    de-reference exported component

--- projects/ng-mat-interactions-dialog/src/lib/ng-mat-interactions-dialog.module.ts
+++ projects/ng-mat-interactions-dialog/src/lib/ng-mat-interactions-dialog.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms';
-import { NgMatInteractionsDialogComponent } from './ng-mat-interactions-dialog.component';
 import { MatButtonModule, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material';
 import { NgMatAlertComponent } from './ng-mat-alert/ng-mat-alert.component';
 import { NgMatConfirmComponent } from './ng-mat-confirm/ng-mat-confirm.component';
 import { NgMatPromptComponent } from './ng-mat-prompt/ng-mat-prompt.component';
 
 @NgModule({
-  declarations: [NgMatInteractionsDialogComponent, NgMatAlertComponent, NgMatConfirmComponent, NgMatPromptComponent],
+  declarations: [NgMatAlertComponent, NgMatConfirmComponent, NgMatPromptComponent],
   imports: [CommonModule, FormsModule, MatButtonModule, MatDialogModule ],
-  exports: [NgMatInteractionsDialogComponent],
   providers: [ {provide: MAT_DIALOG_DATA, useValue: {} }],
   entryComponents: [NgMatAlertComponent, NgMatConfirmComponent, NgMatPromptComponent]
 })
